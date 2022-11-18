5a6b50adbc99633ec2fa11b62cf3049f7f0bfdac
Bruno Cardoso <brunocardoso@sapo.cv>
2022-11-18 11:37:06 -0100

    add+

--- src/screen/dial/Dial.js
+++ src/screen/dial/Dial.js
@@ -1,8 +1,99 @@
+import { Avatar, Modal, Typography } from "@mui/material"
+import { deepOrange, green } from '@mui/material/colors';
+import PhoneForwardedIcon from '@mui/icons-material/PhoneForwarded';
+import BackspaceIcon from '@mui/icons-material/Backspace';
+import './Dial.css';
+import { useState } from "react";
+import { Box } from "@mui/system";
+
 
 function Dial(){
 
+    const [display, setDisplay] = useState([])
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+  
+
+
+    let add = e => {
+
+        display.push(e.target.textContent)
+        setDisplay([...display])
+    
+
+    }
+
     return <>
-        <h1>Dial Screen</h1>
+        <div className="container">
+        
+            <div className="display">
+                <div>
+                    {display.map(task => <span>{task}</span> )}
+                </div>
+                <div>
+                    <  BackspaceIcon/>
+                </div>
+
+             </div>
+
+            <div className="flex">
+                <Avatar onClick={(e)=> add(e)} sx={{ bgcolor: deepOrange[500] }}>1</Avatar>
+                <Avatar onClick={(e)=> add(e)} sx={{ bgcolor: deepOrange[500] }}>2</Avatar>
+                <Avatar onClick={(e)=> add(e)} sx={{ bgcolor: deepOrange[500] }}>3</Avatar>
+            </div>
+            <div className="flex">
+                <Avatar onClick={(e)=> add(e)} sx={{ bgcolor: deepOrange[500] }}>4</Avatar>
+                <Avatar onClick={(e)=> add(e)} sx={{ bgcolor: deepOrange[500] }}>5</Avatar>
+                <Avatar onClick={(e)=> add(e)} sx={{ bgcolor: deepOrange[500] }}>6</Avatar>
+            </div>
+            <div className="flex">
+                <Avatar onClick={(e)=> add(e)} sx={{ bgcolor: deepOrange[500] }}>7</Avatar>
+                <Avatar onClick={(e)=> add(e)} sx={{ bgcolor: deepOrange[500] }}>8</Avatar>
+                <Avatar onClick={(e)=> add(e)} sx={{ bgcolor: deepOrange[500] }}>9</Avatar>
+            </div>
+            <div className="flex">
+                <Avatar onClick={(e)=> add(e)} sx={{ bgcolor: deepOrange[500] }}>*</Avatar>
+                <Avatar onClick={(e)=> add(e)} sx={{ bgcolor: deepOrange[500] }}>0</Avatar>
+                <Avatar onClick={(e)=> add(e)} sx={{ bgcolor: deepOrange[500] }}>#</Avatar>
+            </div>
+            <div className=" flex flex-one">
+            <Avatar onClick={handleOpen} sx={{width: 50, height: 50, bgcolor: green[500] }}>
+                <PhoneForwardedIcon />
+            </Avatar>
+
+
+
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+                >
+                <Box className='modal' sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Calling...
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                       {display}
+                    </Typography>
+                </Box>
+            </Modal>
+            </div>
+        </div>
     </>
 }
 
